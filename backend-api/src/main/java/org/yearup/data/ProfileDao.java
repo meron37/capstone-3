959bb6d8b940c4be441d3ec12a787bd9c9ae99c0
meron37 <134938986+meron37@users.noreply.github.com>
2025-12-19 09:31:48 -0600

    Updated Profile interface

--- backend-api/src/main/java/org/yearup/data/ProfileDao.java
+++ backend-api/src/main/java/org/yearup/data/ProfileDao.java
@@ -1,9 +1,30 @@
-package org.yearup.data;
+//package org.yearup.data;
+//
+//import org.yearup.models.Profile;
+//
+//public interface ProfileDao
+//{
+//    Profile getByUserId(int userId);
+//
+//    // create blank profile at registration time (optional but your register uses it)
+//    Profile create(Profile profile);
+//
+//    // update profile for the current user
+//    void update(Profile profile);
+//}
 
+
+package org.yearup.data;
 
 import org.yearup.models.Profile;
 
 public interface ProfileDao
 {
+    Profile getByUserId(int userId);
+
+    // create blank profile at registration time
     Profile create(Profile profile);
+
+    // update profile for the current user
+    void update(Profile profile);
 }
