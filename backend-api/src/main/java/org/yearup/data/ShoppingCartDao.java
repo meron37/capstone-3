9d2d881886fdb8a34f394eaaf0372254091c56c6
meron37 <134938986+meron37@users.noreply.github.com>
2025-12-19 08:54:36 -0600

    Updated ShoppingCartDao interface

--- backend-api/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ backend-api/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,20 @@
+
 package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+
+    ShoppingCartItem getItemByUserAndProduct(int userId, int productId);
+
+    void addProductToCart(int userId, int productId);
+
+    void updateProductQuantity(int userId, int productId, int quantity);
+
+    void clearCart(int userId);
 }
+
+
